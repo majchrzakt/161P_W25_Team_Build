14f6ac1cf77f207a7cff274ca5998fbe3d63fde8
Tina L Majchrzak <168696256+majchrzakt@users.noreply.github.com>
2025-03-04 17:13:50 -0800

    Update forest.py

--- forest.py
+++ forest.py
@@ -1,19 +1,21 @@
 from validate import get_valid_choice
-valid_choices = ["run", "fight", "negotiate"]
+
 def explore_forest(path):
     print ()
-    print ("you walk into the "+ (path) + " and encounter a wild animal!")
-    choice = input ("Do you fight, run or try to negotiate?")
+    print ("you walk into the "+ path + " and encounter a wild animal!")
+    print ("Do you fight, run or try to negotiate?")
+    valid_choices = "run fight negotiate"
+    choice = get_valid_choice(valid_choices)
 
-    if choice in valid_choices:
-        if choice == "fight":
-            string = "You fight bravely in th " + (path) + " and win the battle!"
-        elif choice == "run":
-            string = "You run away safely from the"  + (path) +"but the adventure ends here"
+    string = ""
+    if "fight" in choice:
+        string = "You fight bravely in th " + path + " and win the battle!"
+    elif "run" in choice:
+        string = "You run away safely from the "  + path + " but the adventure ends here"
+    elif "negotiate" in choice:
+        string = "You successfully negotiate with the wild animal and continue your journey!"
 
-        else:
-            string = "You successfully negotiate with the wild animal and continue your journey!"
-
+    #print(f"string is {string}")
     return string
 
 
